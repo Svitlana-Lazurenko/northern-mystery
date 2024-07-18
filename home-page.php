b197f479450afa4dddcbc777af81bc71eec0b371
Svitlana Lazurenko <lazurenko.svitlana.v@gmail.com>
2024-07-18 18:17:50 +0300

    Add 404-page and develop home-page

--- home-page.php
+++ home-page.php
@@ -8,4 +8,26 @@ get_header(); ?>
 
 <?php the_content(); ?>
 
+<?php $post_projects = get_posts(array(
+    'post_type' => 'project',
+    'posts_per_page' => 3,
+    'orderby' => 'id',
+    'order' => 'DESC'
+));
+
+foreach ($post_projects as $post) :
+    setup_postdata($post);
+?>
+    <div class="">
+        <h3><?php the_title(); ?></h3>
+        <?php the_content(); ?>
+    </div>
+
+<?php
+endforeach;
+wp_reset_postdata();
+?>
+
+
+
 <?php get_footer(); ?>
