8e164d01c85ea8b2473b15e2b0060d3206e08de7
Svitlana Lazurenko <lazurenko.svitlana.v@gmail.com>
2024-07-20 22:02:40 +0300

    Delete options subpage

--- inc/_acf-options-page.php
+++ inc/_acf-options-page.php
@@ -8,18 +8,6 @@ if (function_exists('acf_add_options_page')) {
         'menu_title' => 'Theme Settings',
         'menu_slug'  => 'theme-general-settings',
         'capability' => 'edit_posts',
-        'redirect' => true
-    ));
-
-    acf_add_options_sub_page(array(
-        'page_title'  => 'Theme Header Settings',
-        'menu_title' => 'Header',
-        'parent_slug' => 'theme-general-settings',
-    ));
-
-    acf_add_options_sub_page(array(
-        'page_title'  => 'Theme Footer Settings',
-        'menu_title' => 'Footer',
-        'parent_slug' => 'theme-general-settings',
+        'redirect' => false
     ));
 }
