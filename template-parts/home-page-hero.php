ed1e5de321c81a21b23ac22e3e12cf4572687da0
Svitlana Lazurenko <svitsvit222@gmail.com>
2024-07-30 23:06:11 +0300

    Develop hero & change colors in svg

--- template-parts/home-page-hero.php
+++ template-parts/home-page-hero.php
@@ -6,28 +6,32 @@
         $specialty = get_field('specialty');
         $greeting = get_field('greeting');
         $photo = get_field('photo-welcome');
+        $resume = get_field('resume', 'option');
         ?>
 
         <?php if ($name <> '') : ?>
-            <div><?php echo $name; ?></div>
+            <div class="hero-name"><?php echo $name; ?></div>
         <?php endif; ?>
 
         <?php if ($specialty <> '') : ?>
-            <div><?php echo $specialty; ?></div>
+            <div class="hero-specialty"><?php echo $specialty; ?></div>
         <?php endif; ?>
 
         <?php if ($greeting <> '') : ?>
-            <div><?php echo $greeting; ?></div>
+            <div class="greeting"><?php echo $greeting; ?></div>
         <?php endif; ?>
 
-        <a href="" download="">Download CV</a>
+        <?php if ($resume <> '') : ?>
+            <a class="btn-primary" download href="<?php echo $resume['url']; ?>">Download CV</a>
+        <?php endif; ?>
 
-
-        <div class="" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/svg/tree.svg'; ?>)">
-            <div class="" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/svg/circle-text.svg'; ?>)">
-                <?php if ($photo <> '') : ?>
-                    <img src="<?php echo $photo['url']; ?>" alt="<?php echo $photo['alt']; ?>" />
-                <?php endif; ?>
+        <div class="hero-img">
+            <div class="hero-img__tree" style="width: 800px; height: 800px; background-image: url(<?php echo get_template_directory_uri() . '/assets/svg/tree.svg'; ?>); background-size: cover;">
+                <div class="hero-img__lettering" style="background-image: url(<?php echo get_template_directory_uri() . '/assets/svg/circle-text.svg'; ?>)">
+                    <?php if ($photo <> '') : ?>
+                        <img class="hero-img__photo" src="<?php echo $photo['url']; ?>" alt="<?php echo $photo['alt']; ?>" />
+                    <?php endif; ?>
+                </div>
             </div>
         </div>
 
