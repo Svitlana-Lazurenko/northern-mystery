9f57264dd9c90cb2f72a48db9198d5d88dd7913f
Svitlana Lazurenko <svitsvit222@gmail.com>
2024-08-13 23:32:27 +0300

    Develop projects-page, fix home-page

--- archive-project.php
+++ archive-project.php
@@ -8,11 +8,22 @@
             <?php if (have_posts()) : ?>
                 <ul class="projects__list">
                     <?php while (have_posts()) : the_post(); ?>
-                        <li class="project-card" style="background-image: url('<?php the_post_thumbnail_url(get_the_ID(), 'thumbnail'); ?>');">
+                        <li class="project-card">
                             <a class="project-card__link" href="<?php the_permalink(); ?>">
-                                <h2 class="project-card__title"><?php the_title(); ?></h2>
-                                <p class="project-card__description"><?php the_excerpt(); ?></p>
+                                <div class="project-card__background" style="background-image: url('<?php the_post_thumbnail_url(get_the_ID(), 'thumbnail'); ?>');"></div>
+                                <div class="project-card__overlay">
+                                    <div class="project-card__description"><?php the_excerpt(); ?></div>
+                                </div>
                             </a>
+
+
+                            <h2 class="project-card__title">
+                                <a class="project-card__title-link" href="<?php the_permalink(); ?>">
+                                    <?php the_title(); ?>
+                                </a>
+                            </h2>
+
+
                         </li>
                     <?php endwhile; ?>
                 </ul>
